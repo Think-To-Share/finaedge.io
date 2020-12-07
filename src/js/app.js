ce3a5713ae2ffa8346f63cb3bcfda890ab9bcbff
Abir D <61226387+Abir-Das@users.noreply.github.com>
2020-12-07 20:24:03 +0530

    design completed

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ require('bootstrap');
 
 document.querySelector('.burger-nav #menu').addEventListener('click', () =>{
     document.querySelector("#menu").classList.toggle("change");
-    document.querySelector("#nav").classList.toggle("change");
+    document.querySelector(".fe-nav").classList.toggle("change");
 
     document.querySelector("#menu-bg").classList.toggle("change-bg");
     // document.querySelector('body').classList.toggle('nav-menu-opened')
